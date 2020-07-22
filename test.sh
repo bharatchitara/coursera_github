86b0d8cf3c87b6b42b92ea24b2516bbc81016e73
bharat chitara <bharatchitara99@gmail.com>
2020-07-22 17:35:57 +0530

    added subprocess

--- test.sh
+++ test.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+import os 
+import subprocess
 
 print('hello')
 
 var1=input("enter 1st no.")
 var2 = input("enter 2nd no.")
+
+subprocess.run(['sleep','3'])
 
 var3= int(var1)+int(var2)
 print(var3)
